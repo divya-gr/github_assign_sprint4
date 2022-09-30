8e87620569e9316b64d294ce8fa3d3850cd143b6
Divya <gargdivya704@gmail.com>
2022-09-30 11:51:45 +0530

    some

--- implementation/backend.tf
+++ implementation/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    # bucket = "my-git-buck"
-    # key    = "hit.tfstate"
-    # region = "us-east-1"
-    # dynamodb_table = "Github-table"
+    bucket = "my-git-buck"
+    key    = "hit.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "Github-table"
   }
 }
