8cf26af3814809b28924db7ccbe0512a53c55de2
Divya <111332116+divya-gr@users.noreply.github.com>
2023-10-18 23:57:27 +0530

    Update backend.tf

--- implementation/backend.tf
+++ implementation/backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket = "pinkesh-batch-9"
-    key    = "hit.tfstate"
-    region = "ap-south-1"
-    dynamodb_table = "divya_batch9_training"
+#terraform {
+ # backend "s3" {
+  #  bucket = "pinkesh-batch-9"
+   # key    = "hit.tfstate"
+   # region = "ap-south-1"
+    #dynamodb_table = "divya_batch9_training"
 
-  }
-}
+  #}
+#}
